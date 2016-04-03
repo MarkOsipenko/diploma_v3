70af1173fef80f464d120b8ded8a0775d1b0cebc
MarkOsipenko <mark@osipenko.in.ua>
2016-04-03 20:36:33 +0300

    add actions to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,18 @@
 class CategoriesController < ApplicationController
+  before_action :find_category, only: :show
+  
+  def index
+    @categories = Categories.all
+  end
+
+  def show
+    @pages = @category.pages
+  end
+
+  protected
+
+    def find_category
+      @category = Category.find(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :page_links
   resources :pages
   resources :words
+  resources :categories
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
