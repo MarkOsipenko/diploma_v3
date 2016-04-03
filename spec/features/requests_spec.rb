a67dd689b9487f455cd668de6116fde85a6754a3
MarkOsipenko <mark@osipenko.in.ua>
2016-04-03 21:59:14 +0300

    tests for categories

--- spec/features/requests_spec.rb
+++ spec/features/requests_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Page", type: :feature do
+  before { allow_any_instance_of(Page).to receive(:find_links).and_return(["arr"]) }
   let!(:custom_page) { Page.custom_create("https://ru.wikipedia.org/wiki/Гринвичский_парк") }
+
   subject { page }
 
   context "visit single page" do
@@ -14,4 +16,17 @@ RSpec.describe "Page", type: :feature do
     it { is_expected.to have_content(PageLink.count) }
   end
 
+  context "visit categories_path" do
+    before { visit categories_path }
+    it { is_expected.to have_link("Гринвич") }
+    it { is_expected.to have_link("Парки Лондона") }
+    it { is_expected.to have_link("Сооружения летних Олимпийских игр 2012 года") }
+
+    context "visit one category" do
+      let(:cat) { Category.first }
+      before { visit category_path(cat) }
+
+      it { is_expected.to have_link("Гринвичский парк") }
+    end
+  end
 end
